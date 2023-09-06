14c266b46a43ec1b3057587b7a3a6c32518e71d4
iamedddie <edyolisa@gmail.com>
2023-09-06 22:52:20 +0100

    final dice roller

--- src/main/java/org/example/DiceRoll.java
+++ src/main/java/org/example/DiceRoll.java
@@ -1,13 +1,56 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class DiceRoll {
     public static void main(String[] args) {
-        int[] diceNums = {1, 2, 3, 4, 5, 6};
+        DiceRoller Roller = new DiceRoller();
+    }
+}
 
-        int max = 6;
-        int min = 1;
-        int range = max - min + 1;
-        int roll = (int) (Math.random() * range) + min;
-            System.out.println(roll);
+class DiceRoller {
+    DiceRoller() {
+        Random random = new Random();
+        int number = 0;
+        Roll(random, number);
+
+    }
+
+    public void Roll(Random random, int result) {
+        Scanner input = new Scanner(System.in);
+        String answer = "";
+        int result2 = 0;
+        int i = 0;
+        while (answer.isBlank() || !answer.equalsIgnoreCase("y")) {
+            System.out.println("Throw ?");
+            String response = input.nextLine();
+            if (response.equalsIgnoreCase("y")) {
+                i++;
+                if (i == 1) {
+                    result = random.nextInt(6) + 1;
+                    System.out.println("The lucky number is " + result + " roll the dice again !!!😏");
+                } else {
+                    result2 = random.nextInt(6) + 1;
+                    System.out.println(result2);
+                    System.out.println("Try(s):" + i);
+                }
+                if (result == result2 && i > 2) {
+                    System.out.println("Unlucky, 😂 You rolled the dice " + i + " times to get a " + result);
+                    break;
+                } else if (result == result2 && i <= 2) {
+                    System.out.println("Wow 😮, You rolled the dice just " + i + " time(s) to get a " + result);
+                    break;
+                }
+            }
+
         }
     }
+}
+
+
+
+
+
+
+
